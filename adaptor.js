d5259e7c87d70d2b4c9df7244f41dcbd8ea1656c
Serge Ilinyh <serge.ilinyh@wari.com>
2018-04-23 19:01:24 +0700

    " -> '

--- adaptor.js
+++ adaptor.js
@@ -868,7 +868,7 @@ function transform(api, defaults, callback) {
                     model.allowableValues.enumVars = [];
                     model["allowableValues.values"] = schema.enum;
                     for (let v of schema.enum) {
-                        let e = { name: v, value: '"'+v+'"' }; // insane, why aren't the quotes in the template?
+                        let e = { name: v, value: '\''+v+'\'' }; // insane, why aren't the quotes in the template?
                         model.allowableValues.enumVars.push(e);
                     }
                     model.allowableValues.enumVars = convertArray(model.allowableValues.enumVars);
