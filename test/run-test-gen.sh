a4de406737706bf8044144c93fb676fdea4bc3fe
Serge Ilinyh <serge.ilinyh@wari.com>
2018-04-25 21:18:08 +0700

    test on complex schema

--- test/run-test-gen.sh
+++ test/run-test-gen.sh
@@ -6,8 +6,8 @@ node ../cg.js \
   --templates ../templates/typescript-angular \
   --output generated \
   ../configs/typescript-angular \
-  petstore3.json
+  AMALYZEAG_Falcon_0.1.12_swagger.yaml
 
-rm -r samples/typescript-angular-v5/npm/**
-cp -r generated/typescript-angular/** samples/typescript-angular-v5/npm/
-cp -r generated/typescript-angular/.??** samples/typescript-angular-v5/npm/
+#rm -r samples/typescript-angular-v5/npm/**
+#cp -r generated/typescript-angular/** samples/typescript-angular-v5/npm/
+#cp -r generated/typescript-angular/.??** samples/typescript-angular-v5/npm/
